35034bf45518548a1a864ccfc0d71583e41191be
Jeyavarthini <jeyavarthiniravi@gmail.com>
2023-10-07 15:47:35 +0530

    commit

---
+++ Module 5/problem_with_var.js
@@ -0,0 +1,28 @@
+// var a=39
+// var a=90//redeclaration of variable
+// console.log(a)
+
+// var a=30
+
+// if(a===30)
+// {
+//     var b=67
+//     console.log(b)
+// }
+
+// console.log(b)
+
+// // variable declared with  var are not block scoped
+
+
+function test()
+{
+    var c=90
+    console.log(c)
+}
+
+test()
+
+console.log(c)
+
+// varible declared with var  are functional scoped
