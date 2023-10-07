b9afbdf45841be025fe573838c90440412c53182
Jeyavarthini <jeyavarthiniravi@gmail.com>
2023-10-07 16:08:28 +0530

    commit

---
+++ Module 5/var_const.js
@@ -0,0 +1,21 @@
+//redeclaration is not possible
+
+// let a=20
+// a=90    //reassigning the variable
+// console.log(a)
+
+
+//scope within the block
+
+// if(true)
+// {
+//     let b=29
+//     console.log(b)
+// }
+
+// console.log(b)
+
+const d=12
+//const d=90
+// d=29
+console.log(d)
